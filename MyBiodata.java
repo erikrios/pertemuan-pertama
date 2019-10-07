a69730e1a06a0fa06678fc506ae10b8caa30dd4f
warokdroid <55020861+warokdroid@users.noreply.github.com>
2019-10-07 18:47:27 +0700

    Creaate BiodataSaya.java

---
+++ MyBiodata.java
@@ -0,0 +1,19 @@
+public class MyBiodata {
+
+    public static void main(String[] args) {
+        String nama;
+        int umur;
+        String alamat;
+
+        nama = "Erik Rio Setiawan";
+        umur = 22;
+        alamat = "Bungkal, Ponorogo";
+
+        final String TANGGAL_LAHIR = "2 September 1997";
+
+        System.out.println("Nama saya adalah " + nama);
+        System.out.print("Saya berumur " + umur + "tahun ");
+        System.out.println("dan beralamat di " + alamat);
+        System.out.println("Selain itu, saya lahir pada tanggal " + TANGGAL_LAHIR);
+    }
+}
