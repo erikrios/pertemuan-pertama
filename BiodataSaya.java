255afcb199bec8d6168983c751ed28c0cf73ad72
Muhammad Faisal <51068715+faisall97@users.noreply.github.com>
2019-10-07 12:10:41 +0700

    menambahkan biodata Muhammad Faisal
    acc aku plz

---
+++ BiodataSaya.java
@@ -0,0 +1,22 @@
+public class BiodataSaya {
+
+    public static void main(String[] args){
+
+        String  nama;
+        int umur;
+        String alamat;
+        final String LAHIR;
+
+        nama ="Muhammad Faisal";
+        umur = 21;
+        alamat = "Desa Sewulan, Kecamatan Dagangan, Madiun";
+        LAHIR = "09 Oktober 1997";
+
+        System.out.println("\n=====BIODATA SAYA=====\n");
+        System.out.println("Nama Saya: " + nama);
+        System.out.println("Umur Saya: " + umur + " tahun");
+        System.out.println("Alamat Saya: " + alamat);
+        System.out.println("Selain itu Saya lahir pada " + LAHIR);
+    }
+
+}
